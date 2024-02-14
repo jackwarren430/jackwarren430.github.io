d5a7814b99b3fb42f6fdf1c8d1e954ce9b1c4834
jackwarren430 <90930942+jackwarren430@users.noreply.github.com>
2024-02-14 18:16:35 -0500

    changing to personal website

--- javascript/main.js
+++ javascript/main.js
@@ -1,51 +1,12 @@
-var image = document.querySelector('.arrow-img');
 
 
-image.addEventListener('click', function() {
+var arrow_image = document.querySelector('.arrow-img');
+arrow_image.addEventListener('click', function() {
   window.scrollTo(0, 800);
 });
 
-const header = document.getElementById("header");
-const subheader = document.getElementById("subheader");
-const text = "This is CB Media:";
-let index = 0;
 
-function type() {
-  header.innerHTML += text[index];
-  index++;
-  if (index == text.length) {
-    header.innerHTML = text;
-    setTimeout(function() {
-      subheader.style.display = "block";
-      subheader.innerHTML = "";
-      typeSubheader();
-    }, 500);
-  } else {
-    setTimeout(type, 100);
-  }
-}
-
-function typeSubheader() {
-  const subtext = "We make you seen on the internet.";
-  let subIndex = 0;
-
-  function subType() {
-    subheader.innerHTML += subtext[subIndex];
-    subIndex++;
-    if (subIndex == subtext.length) {
-      subheader.innerHTML = subtext;
-    } else {
-      setTimeout(subType, 50);
-    }
-  }
-  header.classList.add('filler');
-  subType();
-}
-
-type();
-
-
-const slideInElements = document.querySelectorAll('.block');
+const slideInElements = document.querySelectorAll('.text-block');
 
 const slideInOptions = {
   threshold: 0.5,
